fc547fc97bd6325bdc0f0706ec266a19667d7be8
TannerEsch <eschmanntanner@gmail.com>
2022-09-10 18:10:14 -0400

    isLoggedIn: add middleware and import to server

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app = express();
 const session = require('express-session');
 const flash = require('connect-flash');
 const passport = require('./config/ppConfig');
+const isLoggedIn = require('./middleware/isLoggedIn');
+
 
 
 const SECRET_SESSION = process.env.SECRET_SESSION;
