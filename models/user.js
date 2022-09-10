9ce178cf00e19f943c608c1c8eac47fbbad92054
TannerEsch <eschmanntanner@gmail.com>
2022-09-10 15:58:21 -0400

    add: user model and validations

---
+++ models/user.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class user extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  user.init({
+    name: {
+      type: DataTypes.STRING,
+      validate: {
+       len: {
+        args: [1,99],
+        msg: 'Name must be between 1 and 99 characters'
+       }
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          msg: 'Invalid email'
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [8,99],
+          msg: 'Password must be between 8 and 99 characters'
+        }
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'user',
+  });
+
+  return user; // add functions above 
+};
